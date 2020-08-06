e4fa4722ecbdd77ee288bff8e9680a4d252c39d4
Julius Müller <julius.matthias.mueller@gmail.com>
2020-08-06 18:17:14 +0200

    Initial Commit

---
+++ Coronaregel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Coronaregel
+//
+//  Created by Mueller, Julius Matthias on 06.08.20.
+//  Copyright © 2020 Mueller, Julius Matthias. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
