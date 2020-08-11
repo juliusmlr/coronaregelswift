90c8f3c7c7357850b20525df67c555e65ab703dd
Julius Müller <julius.matthias.mueller@gmail.com>
2020-08-11 11:24:59 +0200

    v1

---
+++ Coronaregel/Views/StateList/StateDetails.swift
@@ -0,0 +1,9 @@
+//
+//  StateDetails.swift
+//  Coronaregel
+//
+//  Created by Mueller, Julius Matthias on 06.08.20.
+//  Copyright © 2020 Mueller, Julius Matthias. All rights reserved.
+//
+
+import Foundation
